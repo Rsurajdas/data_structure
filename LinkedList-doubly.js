68583e398de4ae872d9d808280d6a2fc911ed3d7
suraj <surajkumar18294@gmail.com>
2023-06-03 10:39:56 +0530

    doubly linked list

--- LinkedList-doubly.js
+++ LinkedList-doubly.js
@@ -1,20 +1,28 @@
 class Node {
-  constructor(value, next) {
+  constructor(value, next, prev) {
     this.value = value;
     this.next = next;
+    this.prev = prev;
   }
 }
 
 class LinkedList {
   constructor() {
-    this.front = new Node(null, null);
+    this.front = new Node(null, null, null);
+    this.rear = new Node(null, null, null);
     this.size = 0;
   }
 
   // initially front is null
   insertFront(value) {
-    const newNode = new Node(value, this.front);
-    this.front = newNode;
+    const newNode = new Node(value, null, null);
+    if (this.size === 0) {
+      this.front = this.rear = newNode;
+    } else {
+      newNode.next = this.front;
+      this.front.prev = newNode;
+      this.front = newNode;
+    }
     this.size++;
   }
 
@@ -26,7 +34,7 @@ class LinkedList {
   print() {
     let holder = this.front;
     // console.log(holder);
-    while (holder.next !== null) {
+    while (holder !== null) {
       console.log(holder.value);
       holder = holder.next;
     }
@@ -46,8 +54,10 @@ class LinkedList {
           holder = holder.next;
           i++;
         }
-
-        const newNode = new Node(value, holder.next);
+        const newNode = new Node(value, holder.next, holder);
+        if (holder.next !== null) {
+          holder.next.prev = newNode;
+        }
         holder.next = newNode;
         this.size++;
       }
@@ -68,6 +78,7 @@ class LinkedList {
           holder = holder.next;
           i++;
         }
+        holder.next.next.prev = holder;
         holder.next = holder.next.next;
         this.size--;
       }
@@ -77,18 +88,28 @@ class LinkedList {
   length() {
     console.log(this.size);
   }
+
+  printReverse() {
+    var holder = this.rear;
+
+    while (holder.prev != null) {
+      console.log(holder.value);
+      holder = holder.prev;
+    }
+  }
 }
 
 let list = new LinkedList();
 list.insertFront(10);
-
 list.insertFront(20);
 list.insertFront(30);
 list.insertFront(40);
 list.insert(50, 3);
-list.insert(60, 4);
+list.insert(60, 2);
 list.deleteFront();
-
 list.delete(3);
+list.printReverse();
+list.length();
+console.log("--------------------------------");
 list.print();
 list.length();
