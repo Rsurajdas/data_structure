5fffe9cf74763715f5e07d6e0d10fd2133709818
Rsurajdas <surajkumar18294@gmail.com>
2023-06-04 09:30:20 +0530

    Hash table remove duplicate and find intercept fn added

--- HashTable.js
+++ HashTable.js
@@ -53,4 +53,38 @@ class HashTable {
   }
 }
 
-let hash = new HashTable(20);
+let hash = new HashTable(251);
+let arr = [0, 1, 1, 2, 2, 3, 4, 5, 5, 6, 6, 7, 8, 8, 9];
+let result = [];
+
+for (let i = 0; i < arr.length; i++) {
+  let val = arr[i];
+  if (!hash.contains(val)) {
+    hash.add(val);
+    result.push(val);
+  }
+}
+
+console.log(result);
+
+let hash2 = new HashTable(251);
+let arr1 = [1, 2, 4, 2, 5, 7, 9, 3, 18, 0, 19];
+let arr2 = [3, 2, 5, 10, 1, 15, 1];
+
+function intercept(arr1, arr2) {
+  let result = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (!hash2.contains(arr1[i])) {
+      hash2.add(arr1[i]);
+    }
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    if (hash2.contains(arr2[i])) {
+      result.push(arr2[i]);
+    }
+  }
+  return result;
+}
+
+console.log(intercept(arr1, arr2));
